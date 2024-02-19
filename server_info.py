07396bd64c0e132678944bcf09f91f8326137d89
Fedor Gorlenko <fedorgorlenko@gmail.com>
2024-02-19 19:50:58 +0300

    bug fix

--- server_info.py
+++ server_info.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-con = sqlite3.connect('./config.sqlite3')
+con = sqlite3.connect('./config.db')
 cur = con.cursor()
 password = str(cur.execute('''SELECT znach FROM settings
                             WHERE name == "password"''').fetchone()[0])
